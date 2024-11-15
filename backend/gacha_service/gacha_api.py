9885d79bd2bb2c82c855df7f1e43609de0826a31
funesca <funa.france@gmail.com>
2024-11-15 19:15:40 +0100

    gacha player endpoints

--- backend/gacha_service/gacha_api.py
+++ backend/gacha_service/gacha_api.py
@@ -4,6 +4,9 @@ import os
 from decode_auth_token import decode_token, token_required
 
 app = Flask(__name__)
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for Flask application")
 
 # Get Connection DB
 def get_db_connection():
@@ -14,20 +17,27 @@ def get_db_connection():
         database=os.environ.get("DB_NAME")
     )
 
-@app.route('/collection/<int:userId>', methods=['GET'])
-#@token_required(role_required='Player')
-def get_user_collection(userId):
-    # check authorization (TODO)
+@app.route('/collection', methods=['GET'])
+@token_required(role_required='Player')
+def get_user_collection():
     
+    token = request.headers.get('Authorization', '').replace('Bearer ', '')
+    user_data = decode_token(token)
+    if not user_data:
+        return jsonify(), 401
+    user_id = user_data.get('user_id')
+    if not  user_id:
+        return jsonify(), 401
+
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
-    query = """
+    query = """ 
     SELECT Gacha.id, Gacha.name, Gacha.description, Gacha.id_img, Gacha.rarity, Collection.quantity
     FROM Collection
     JOIN Gacha ON Collection.gacha_id = Gacha.id
     WHERE Collection.user_id=%s
     """
-    cursor.execute(query, (userId, ))
+    cursor.execute(query, (user_id, ))
     user_collection = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -36,10 +46,17 @@ def get_user_collection(userId):
         return jsonify([]), 200
     return jsonify(user_collection), 200
 
-@app.route('/collection/<int:userId>/<int:gachaId>', methods=['GET'])
-#@token_required(role_required='Player')
-def get_user_gacha(userId, gachaId):
-    # check authorization (TODO)
+@app.route('/collection/<int:gacha_id>', methods=['GET']                )
+@token_required(role_required='Player')
+def get_user_gacha(gacha_id):
+
+    token = request.headers.get('Authorization', '').replace('Bearer ', '')
+    user_data = decode_token(token)
+    if not user_data:
+        return jsonify(), 401
+    user_id = user_data.get('user_id')
+    if not  user_id:
+        return jsonify(), 401
 
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
@@ -49,7 +66,7 @@ def get_user_gacha(userId, gachaId):
     JOIN Gacha ON Collection.gacha_id = Gacha.id
     WHERE Collection.user_id = %s AND Collection.gacha_id = %s;
     """
-    cursor.execute(query, (userId, gachaId))
+    cursor.execute(query, (user_id, gacha_id))
     user_gacha = cursor.fetchone()
     cursor.close()
     connection.close()
@@ -58,10 +75,17 @@ def get_user_gacha(userId, gachaId):
         return jsonify({"error": "Gacha not found or not owned by user."}), 404
     return jsonify(user_gacha), 200
 
-@app.route('/collection/<int:userId>/available', methods=['GET'])
-#@token_required(role_required='Player')
-def get_available_gacha(userId):
-    # check authorization (TODO)    
+@app.route('/collection/available', methods=['GET'])
+@token_required(role_required='Player')
+def get_available_gachas():
+
+    token = request.headers.get('Authorization', '').replace('Bearer ', '')
+    user_data = decode_token(token)
+    if not user_data:
+        return jsonify(), 401
+    user_id = user_data.get('user_id')
+    if not  user_id:
+        return jsonify(), 401
 
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
@@ -71,7 +95,7 @@ def get_available_gacha(userId):
     LEFT JOIN Collection ON Gacha.id = Collection.gacha_id AND Collection.user_id = %s 
     WHERE Collection.user_id IS NULL;
     """
-    cursor.execute(query, (userId, ))
+    cursor.execute(query, (user_id, ))
     available_gacha = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -79,21 +103,28 @@ def get_available_gacha(userId):
     if not available_gacha or available_gacha is None:
         return jsonify({"message": "User owns every gacha."}), 200
     return jsonify(available_gacha), 200
-'''
-@app.route('/collection/<int:userId>/available/<int:gachaId>', methods=['GET'])
-#@token_required(role_required='Player')
-def get_available_gacha(userId, gachaId):
-    # check authorization (TODO)    
+
+@app.route('/collection/available/<int:gacha_id>', methods=['GET'])
+@token_required(role_required='Player')
+def get_available_gacha(gacha_id):
+
+    token = request.headers.get('Authorization', '').replace('Bearer ', '')
+    user_data = decode_token(token)
+    if not user_data:
+        return jsonify(), 401
+    user_id = user_data.get('user_id')
+    if not  user_id:
+        return jsonify(), 401
 
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
     query = """
     SELECT Gacha.id, Gacha.name, Gacha.description, Gacha.id_img, Gacha.rarity
     FROM Gacha
-    LEFT JOIN Collection ON Gacha.id = Collection.gacha_id AND Collection.user_id = %s 
-    WHERE Collection.user_id IS NULL;
+    LEFT JOIN Collection ON Gacha.id = Collection.gacha_id AND Collection.user_id = %s
+    WHERE Gacha.id = 2 AND Collection.user_id IS NULL;
     """
-    cursor.execute(query, (userId, ))
+    cursor.execute(query, (user_id, ))
     available_gacha = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -101,6 +132,6 @@ def get_available_gacha(userId, gachaId):
     if not available_gacha or available_gacha is None:
         return jsonify({"message": "User owns every gacha."}), 200
     return jsonify(available_gacha), 200
-'''
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
